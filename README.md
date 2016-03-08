ece756db87bdf97a59a6d8724be29bfc05245c8c
William Marques <wmarques@ippon.fr>
2016-03-08 15:05:39 +0100

    scanning directories and finding jhipster apps

---
+++ README.md
@@ -0,0 +1,33 @@
+# generator-docker-jhipster [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+> Generate Docker to run JHipster apps
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-docker-jhipster using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-docker-jhipster
+```
+
+Then generate your new project:
+
+```bash
+yo docker-jhipster
+```
+
+## Getting To Know Yeoman
+
+Yeoman has a heart of gold. He&#39;s a person with feelings and opinions, but he&#39;s very easy to work with. If you think he&#39;s too opinionated, he can be easily convinced. Feel free to [learn more about him](http://yeoman.io/).
+
+## License
+
+Apache-2.0 © [William Marques]()
+
+
+[npm-image]: https://badge.fury.io/js/generator-docker-jhipster.svg
+[npm-url]: https://npmjs.org/package/generator-docker-jhipster
+[travis-image]: https://travis-ci.org//generator-docker-jhipster.svg?branch=master
+[travis-url]: https://travis-ci.org//generator-docker-jhipster
+[daviddm-image]: https://david-dm.org//generator-docker-jhipster.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org//generator-docker-jhipster
---
+++ generators/app/index.js
@@ -0,0 +1,55 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+var shelljs = require('shelljs');
+
+
+module.exports = yeoman.generators.Base.extend({
+    initializing: {
+        findJhipsterApps: function () {
+            var files = shelljs.ls('-l',this.destinationRoot());
+            this.apps = [];
+            files.forEach(function(file) {
+                if(file.isDirectory()) {
+                    if(shelljs.test('-f', file.name + '/.yo-rc.json')) {
+                        this.apps.push(file.name.match(/([^\/]*)\/*$/)[1]);
+                    }
+                }
+            }, this);
+        }
+    },
+    prompting: function () {
+        var done = this.async();
+
+        // Have Yeoman greet the user.
+        this.log(yosay(
+            'Welcome to the first-rate ' + chalk.red('generator-docker-jhipster') + ' generator!'
+        ));
+
+        var prompts = [{
+            type: 'checkbox',
+            name: 'chosenApps',
+            message: 'Which app do you want in your DockerFile ?',
+            choices: this.apps
+        }];
+
+        this.prompt(prompts, function (props) {
+            this.props = props;
+            // To access props later use this.props.someOption;
+
+            done();
+        }.bind(this));
+    },
+
+    writing: function () {
+        this.fs.copy(
+          this.templatePath('dummyfile.txt'),
+          this.destinationPath('dummyfile.txt')
+        );
+    },
+
+    install: function () {
+        this.installDependencies();
+    }
+});
