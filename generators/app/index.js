5659cb0d8901a974fce4f3ebccdcfaf889ed75ba
William Marques <wmarques@ippon.fr>
2016-03-09 11:14:25 +0100

    adding eslint config, loading yo-rc applications config, removing yosay from npm depedencies

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,12 +1,23 @@
 'use strict';
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
-var yosay = require('yosay');
 var shelljs = require('shelljs');
 
 module.exports = yeoman.generators.Base.extend({
     initializing: {
-        findJhipsterApps: function () {
+        printJHipsterLogo: function() {
+            this.log(' \n' +
+                chalk.green('        ██') + chalk.red('  ██    ██  ████████  ███████    ██████  ████████  ████████  ███████\n') +
+                chalk.green('        ██') + chalk.red('  ██    ██     ██     ██    ██  ██          ██     ██        ██    ██\n') +
+                chalk.green('        ██') + chalk.red('  ████████     ██     ███████    █████      ██     ██████    ███████\n') +
+                chalk.green('  ██    ██') + chalk.red('  ██    ██     ██     ██             ██     ██     ██        ██   ██\n') +
+                chalk.green('   ██████ ') + chalk.red('  ██    ██  ████████  ██        ██████      ██     ████████  ██    ██\n'));
+            this.log(chalk.white.bold('                            http://jhipster.github.io\n'));
+            this.log(chalk.white('Welcome to the JHipster Docker Compose Generator '));
+            this.log(chalk.white('Files will be generated in folder: ' + chalk.yellow(this.destinationRoot())));
+        },
+
+        findJhipsterApps: function() {
             var files = shelljs.ls('-l',this.destinationRoot());
             this.apps = [];
             files.forEach(function(file) {
@@ -16,17 +27,23 @@ module.exports = yeoman.generators.Base.extend({
                     }
                 }
             }, this);
+
+            if(this.apps.length === 0) {
+                this.isEmpty = true;
+                this.log(chalk.red('\nNo applications found in ' + this.destinationRoot()));
+                this.log(chalk.red('\nMake sure you run the generator in a directory containing JHipster generated applications'));
+                return;
+            } else {
+                this.isEmpty = false;
+                this.log(chalk.green(this.apps.length + ' applications found at ' + this.destinationRoot() + '\n'));
+            }
         }
     },
 
     prompting: {
-        askForApps: function () {
+        askForApps: function() {
+            if(this.isEmpty) return;
             var done = this.async();
-
-            // Have Yeoman greet the user.
-            this.log(yosay(
-                'Welcome to the first-rate ' + chalk.red('generator-docker-jhipster') + ' generator!'
-            ));
 
             var prompts = [{
                 type: 'checkbox',
@@ -35,7 +52,7 @@ module.exports = yeoman.generators.Base.extend({
                 choices: this.apps,
                 validate: function (input) {
                     if(input.length === 0) {
-                        return "Please choose at least one application";
+                        return 'Please choose at least one application';
                     } else return true;
                 }
             }];
@@ -47,7 +64,8 @@ module.exports = yeoman.generators.Base.extend({
             }.bind(this));
         },
 
-        askForElk: function () {
+        askForElk: function() {
+            if(this.isEmpty) return;
             var done = this.async();
 
             var prompts = [{
@@ -57,7 +75,7 @@ module.exports = yeoman.generators.Base.extend({
                 default: true
             }];
 
-            this.prompt(prompts, function (props) {
+            this.prompt(prompts, function(props) {
                 this.useElk = props.elk;
 
                 done();
@@ -65,18 +83,34 @@ module.exports = yeoman.generators.Base.extend({
         }
     },
 
+    configuring: {
+        getAppConfig: function() {
+            if(this.isEmpty) return;
+
+            this.appConfigs = [];
+            for(var i=0;i<this.apps.length;i++) {
+                var fileData = this.fs.readJSON(this.destinationPath(this.apps[i]+'/.yo-rc.json'));
+                this.appConfigs.push(fileData['generator-jhipster']);
+            }
+        }
+    },
+
     writing: {
-        writeDockerCompose: function () {
+        writeDockerCompose: function() {
+            if(this.isEmpty) return;
+
             this.template('_docker-compose.yml', 'docker-compose.yml');
         },
 
-        writeRegistryFiles: function () {
+        writeRegistryFiles: function() {
+            if(this.isEmpty) return;
+
             this.copy('registry.yml', 'registry.yml');
             this.copy('central-server-config/application.yml', 'central-server-config/application.yml');
         },
 
-        writeElkFiles: function () {
-            if(!this.useElk) return;
+        writeElkFiles: function() {
+            if(!this.useElk || this.isEmpty) return;
 
             this.copy('elk.yml', 'elk.yml');
             this.copy('log-monitoring/log-config/logstash.conf', 'log-monitoring/log-config/logstash.conf');
