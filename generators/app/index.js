9825615f4840a97df18a310de71e6421b85385c0
William Marques <wmarques@ippon.fr>
2016-03-09 12:09:09 +0100

    checking if docker images are generated

--- generators/app/index.js
+++ generators/app/index.js
@@ -19,37 +19,37 @@ module.exports = yeoman.generators.Base.extend({
 
         findJhipsterApps: function() {
             var files = shelljs.ls('-l',this.destinationRoot());
-            this.apps = [];
+            this.appsFolders = [];
             files.forEach(function(file) {
                 if(file.isDirectory()) {
                     if(shelljs.test('-f', file.name + '/.yo-rc.json')) {
-                        this.apps.push(file.name.match(/([^\/]*)\/*$/)[1]);
+                        this.appsFolders.push(file.name.match(/([^\/]*)\/*$/)[1]);
                     }
                 }
             }, this);
 
-            if(this.apps.length === 0) {
-                this.isEmpty = true;
+            if(this.appsFolders.length === 0) {
+                this.abort = true;
                 this.log(chalk.red('\nNo applications found in ' + this.destinationRoot()));
                 this.log(chalk.red('\nMake sure you run the generator in a directory containing JHipster generated applications'));
                 return;
             } else {
-                this.isEmpty = false;
-                this.log(chalk.green(this.apps.length + ' applications found at ' + this.destinationRoot() + '\n'));
+                this.abort = false;
+                this.log(chalk.green(this.appsFolders.length + ' applications found at ' + this.destinationRoot() + '\n'));
             }
         }
     },
 
     prompting: {
         askForApps: function() {
-            if(this.isEmpty) return;
+            if(this.abort) return;
             var done = this.async();
 
             var prompts = [{
                 type: 'checkbox',
                 name: 'chosenApps',
                 message: 'Which applications do you want in your DockerFile ?',
-                choices: this.apps,
+                choices: this.appsFolders,
                 validate: function (input) {
                     if(input.length === 0) {
                         return 'Please choose at least one application';
@@ -58,14 +58,14 @@ module.exports = yeoman.generators.Base.extend({
             }];
 
             this.prompt(prompts, function (props) {
-                this.apps = props.chosenApps;
+                this.appsFolders = props.chosenApps;
 
                 done();
             }.bind(this));
         },
 
         askForElk: function() {
-            if(this.isEmpty) return;
+            if(this.abort) return;
             var done = this.async();
 
             var prompts = [{
@@ -85,32 +85,49 @@ module.exports = yeoman.generators.Base.extend({
 
     configuring: {
         getAppConfig: function() {
-            if(this.isEmpty) return;
+            if(this.abort) return;
 
             this.appConfigs = [];
-            for(var i=0;i<this.apps.length;i++) {
-                var fileData = this.fs.readJSON(this.destinationPath(this.apps[i]+'/.yo-rc.json'));
+            for(var i = 0; i < this.appsFolders.length; i++) {
+                var fileData = this.fs.readJSON(this.destinationPath(this.appsFolders[i]+'/.yo-rc.json'));
                 this.appConfigs.push(fileData['generator-jhipster']);
             }
+        },
+
+        checkImages: function() {
+            if(this.abort) return;
+
+            this.log('\nChecking Docker images in applications directories...');
+
+            for (var i = 0; i < this.appsFolders.length; i++) {
+                var imagePath = this.destinationPath(this.appsFolders[i] + '/target/docker/'+this.appConfigs[i].baseName.toLowerCase()+'-0.0.1-SNAPSHOT.war');
+                if (!shelljs.test('-f', imagePath)) {
+                    this.log(chalk.red('\nDocker Image not found at ' + imagePath));
+                    this.log(chalk.red('Please run "mvn package docker:build" in ' + this.destinationPath(this.appsFolders[i]) + ' to generate Docker image'));
+                    this.abort = true;
+                }
+            }
+
+            if(!this.abort) this.log(chalk.green('Found Docker images, writing files...\n'));
         }
     },
 
     writing: {
         writeDockerCompose: function() {
-            if(this.isEmpty) return;
+            if(this.abort) return;
 
             this.template('_docker-compose.yml', 'docker-compose.yml');
         },
 
         writeRegistryFiles: function() {
-            if(this.isEmpty) return;
+            if(this.abort) return;
 
             this.copy('registry.yml', 'registry.yml');
             this.copy('central-server-config/application.yml', 'central-server-config/application.yml');
         },
 
         writeElkFiles: function() {
-            if(!this.useElk || this.isEmpty) return;
+            if(!this.useElk || this.abort) return;
 
             this.copy('elk.yml', 'elk.yml');
             this.copy('log-monitoring/log-config/logstash.conf', 'log-monitoring/log-config/logstash.conf');
